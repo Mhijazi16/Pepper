a291b2b98e6893ca0ff8d07221c396e2867f9bbc
Mhijazi16 <gultimate0@gmail.com>
2024-06-09 19:31:46 +0300

    Cpp-Compilation : running cpp file

--- pepper.sh
+++ pepper.sh
@@ -5,3 +5,23 @@ if [ $# -eq 0 ]; then
     echo "🚨 Usage: pepper <filename.cpp>"
     exit 1
 fi
+
+# Extract filename without extension
+filename=$(basename -- "$1")
+name="${filename%.*}"
+
+# Compile the .cpp file using g++
+echo "======================="
+echo "🛠️ Compiling $filename..."
+if g++ -o "$name" "$1"; then
+    # Compilation successful, execute the binary
+    echo "🌶️ Running $filename..."
+    echo "======================="
+    "./$name"
+    rm -rf "./$name"
+else
+    # Compilation failed
+    echo "🌵 Compilation failed for $filename"
+fi
+
+
